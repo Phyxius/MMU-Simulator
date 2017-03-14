7782975d07b624fa26809784dbbd5bccfcbd60b7
Shea Polansky <env.laser@gmail.com>
2017-03-14 15:27:31 -0700

    Made coding style for pre-simulation output more consistent

--- Lab 3/Program.cs
+++ Lab 3/Program.cs
@@ -77,15 +77,15 @@ namespace Lab_3
             Console.WriteLine($"Page bits: {p.GetPageBits()}");
             Console.WriteLine($"Offset bits: {p.GetOffsetBits()}");
             Console.WriteLine($"TLB size: {s.TLBSize}");
-            Console.WriteLine("TLB latency (milliseconds): " + s.TLBLatencyMS.ToString("0.000000"));
+            Console.WriteLine($"TLB latency (milliseconds): {s.TLBLatencyMS.ToString("0.000000")}");
             Console.WriteLine($"Physical memory (bytes): {s.PhysicalMemorySize}");
             Console.WriteLine($"Physical frame size (bytes) {s.FrameSize}");
             Console.WriteLine($"Number of physical frames: {p.GetMaxFrames()}");
-            Console.WriteLine("Memory latency (milliseconds): " + s.MemoryLatencyMS.ToString("0.000000"));
+            Console.WriteLine($"Memory latency (milliseconds): {s.MemoryLatencyMS.ToString("0.000000")}");
             Console.WriteLine($"Number of page table entries: {p.GetMaxPages()}");
             Console.WriteLine($"Page replacement strategy: {s.PageReplacementPolicy.GetConfigName()}");
-            Console.WriteLine("Disk latency (milliseconds): " + s.DiskLatency.ToString("0.00"));
-            Console.WriteLine("Logging: " + (s.LoggingOutput ? "on" : "off"));
+            Console.WriteLine($"Disk latency (milliseconds): {s.DiskLatency.ToString("0.00")}");
+            Console.WriteLine($"Logging: {(s.LoggingOutput ? "on" : "off")}");
         }
 
         private static IPageTable CreatePageTable(Settings s)
